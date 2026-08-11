bc02304b5c6f644531715ca221274f9e31a09dff
egornomic <git@egornomic.xyz>
2026-08-11 23:16:27 +0200

    feat: use Instrument Sans for wordmark

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Toaster } from "sonner";
 import "@fontsource-variable/ibm-plex-sans/wght.css";
 import "@fontsource-variable/ibm-plex-sans/wght-italic.css";
+import "@fontsource-variable/instrument-sans/wght.css";
 import { App } from "./App";
 import { isDesktopApp } from "./desktop";
 import { PwaUpdate } from "./pwa-update";
